0b276f077c169b12e2a92ce80cdba7a8d39a0579
曹亚文 <caoyawen@meican.com>
2016-05-18 00:22:47 +0800

    初始化了一些东西

---
+++ Gruntfile.js
@@ -0,0 +1,16 @@
+module.exports = function(grunt){
+  grunt.initConfig(
+    {
+      watch:{
+        
+      }
+    }
+  );
+  
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+};
