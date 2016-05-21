e973564faac0a4394ee13d6b49925023e1402ff3
曹亚文 <caoyawen@meican.com>
2016-05-21 13:18:26 +0800

    准备改简历

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,12 +1,41 @@
 module.exports = function(grunt){
   grunt.initConfig(
     {
+      less:{
+        src:{
+          options:{
+            paths: ["src"],
+            compress: true,
+            sourceMap: true,
+            banner: "/*2016 InteractCSS*/"
+            
+          },
+          files:{
+            "dist_compile/hover.css": "src/hover.less"
+          }
+        }
+      },
       watch:{
-        
+        less:{
+          files: ["src/*.less"],
+          filters: 'isFile',
+          tasks: ["less:src", "concat:less"]
+        }
+      },
+      concat:{
+        options:{
+          sourceMap: true
+        },
+        less:{
+          src:[
+            'dist_compile/*.css'
+          ],
+          dest: "dist/interact_css.css"
+        }
       }
     }
   );
-  
+
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-concat');
